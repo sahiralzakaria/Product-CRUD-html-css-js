304dd2b14c7ba59e03c43d2cf9b790d3184c9e27
sahir zakaria <sahir.alzakaria@gmail.com>
2025-09-14 04:10:39 +0300

    Initial commit - add Product Management System project (HTML, CSS, JS)

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let title = document.getElementById('title');
+let price = document.getElementById('price');
+let taxes = document.getElementById('taxes');
+let ads = document.getElementById('ads');
+let discount = document.getElementById('discount');
+let total = document.getElementById('total');
+let count = document.getElementById('count');
+let category = document.getElementById('category');
+let submit = document.getElementById('submit');
+
+console.log(title, price, taxes, ads, discount, total, count, category, submit)
