18d5f838488f5e6bdfd98f1909b05f2b3ca11b69
sahir zakaria <sahir.alzakaria@gmail.com>
2025-09-15 04:15:18 +0300

    read data function

--- scripts.js
+++ scripts.js
@@ -51,6 +51,7 @@ submit.onclick = function () {
     localStorage.setItem('product', JSON.stringify(dataPro));
 
     clearData();
+    showData();
 }
 
 // clear data
@@ -67,4 +68,30 @@ function clearData() {
     category.value = '';
     total.style.background = '#a00d02';
 
+}
+
+// read data
+
+function showData() {
+    let table = '';
+
+    for (let i = 0; i < dataPro.length; i++) {
+        table += `
+                <tr>
+                        <td>${i + 1}</td>
+                        <td>${dataPro[i].title}</td>
+                        <td>${dataPro[i].price}</td>
+                        <td>${dataPro[i].taxes}</td>
+                        <td>${dataPro[i].ads}</td>
+                        <td>${dataPro[i].discount}</td>
+                        <td>${dataPro[i].total}</td>
+                        <td>${dataPro[i].category}</td>
+                        <td><button id="update">update</button></td>
+                        <td><button id="delete">delete</button></td>
+                </tr>
+        `;
+    }
+
+    document.getElementById('tbody').innerHTML = table;
+    console.log
 }
