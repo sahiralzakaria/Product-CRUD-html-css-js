b2de1c1ebf62a39b21c75b67e855060f2a95ff7d
sahir zakaria <sahir.alzakaria@gmail.com>
2025-09-15 23:16:41 +0300

    search data function

--- scripts.js
+++ scripts.js
@@ -39,14 +39,14 @@ if (localStorage.product != null) {
 submit.onclick = function () {
 
     let newPro = {
-        title: title.value,
+        title: title.value.toLowerCase(),
         price: price.value,
         taxes: taxes.value,
         ads: ads.value,
         discount: discount.value,
         total: total.innerHTML,
         count: count.value,
-        category: category.value,
+        category: category.value.toLowerCase(),
 
     }
 
@@ -159,4 +159,75 @@ function updateData(i) {
         top: 0,
         behavior: 'smooth'
     })
+}
+
+// Search
+
+let SearchMood = 'title';
+function getSearchMood(id) {
+
+    let search = document.getElementById('search');
+
+    if (id == 'searchTitle') {
+        SearchMood = 'title';
+
+    } else {
+        SearchMood = 'category';
+
+    }
+    search.placeholder = 'Search By ' + SearchMood;
+    search.focus();
+    search.value = '';
+    showData();
+
+}
+
+function searchData(value) {
+
+    let table = '';
+
+
+    for (let i = 0; i < dataPro.length; i++) {
+        if (SearchMood == 'title') {
+
+            if (dataPro[i].title.includes(value.toLowerCase())) {
+                table += `
+                <tr>
+                        <td>${i + 1}</td>
+                        <td>${dataPro[i].title}</td>
+                        <td>${dataPro[i].price}</td>
+                        <td>${dataPro[i].taxes}</td>
+                        <td>${dataPro[i].ads}</td>
+                        <td>${dataPro[i].discount}</td>
+                        <td>${dataPro[i].total}</td>
+                        <td>${dataPro[i].category}</td>
+                        <td><button id="update" onclick=updateData(${i})>update</button></td>
+                        <td><button id="delete" onclick=deleteData(${i})>delete</button></td>
+                </tr>
+        `;
+            }
+
+        } else {
+
+            if (dataPro[i].category.includes(value.toLowerCase())) {
+                table += `
+                <tr>
+                        <td>${i + 1}</td>
+                        <td>${dataPro[i].title}</td>
+                        <td>${dataPro[i].price}</td>
+                        <td>${dataPro[i].taxes}</td>
+                        <td>${dataPro[i].ads}</td>
+                        <td>${dataPro[i].discount}</td>
+                        <td>${dataPro[i].total}</td>
+                        <td>${dataPro[i].category}</td>
+                        <td><button id="update" onclick=updateData(${i})>update</button></td>
+                        <td><button id="delete" onclick=deleteData(${i})>delete</button></td>
+                </tr>
+        `;
+            }
+
+        }
+    }
+    document.getElementById('tbody').innerHTML = table;
+
 }
